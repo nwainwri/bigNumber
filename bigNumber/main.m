477291d776367a051f77d542346967df2c19d69d
Nathan Wainwright <nathanwainwright@gmail.com>
2018-07-16 01:21:19 -0700

    Initial Commit

---
+++ bigNumber/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  bigNumber
+//
+//  Created by Nathan Wainwright on 2018-07-16.
+//  Copyright © 2018 Nathan Wainwright. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
